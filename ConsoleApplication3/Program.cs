f8181a6d42a625a77e87f00fbf4ffbe0fd1867b0
DexterIV <wiedzmenpl@gmail.com>
2018-06-11 20:00:36 +0200

    Some resharper, some additional options

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -6,51 +6,54 @@ namespace ConsoleApplication3
 {
     public class Program
     {
-        /// <summary>
-        /// Main
-        /// </summary>
-        /// <param name="args">Not used.</param>
         public static void Main(string[] args)
         {
-            Console.WriteLine("ATChat");
             Console.WriteLine("Przemyslaw Fortuna:210176 Krzysztof Barden: 210139");
-            Console.WriteLine("Avaiable ports:");
-            foreach(var x in SerialPort.GetPortNames()) //pobranie wszystkich dostępnych portów
+            Console.WriteLine("ATChat");
+            while (true)
             {
-                Console.WriteLine(x);
-            }
-            Console.Write("Provide port: ");
-            var input = Console.ReadLine();
-            Console.Write("Provide baud rate: ");
-            int.TryParse(Console.ReadLine(), out int baudrate);
-
-            foreach (var x in SerialPort.GetPortNames()) //przeszukanie czy wprowadzony port znajduje sie wsrod dostepnych portow
-            {
-                if (x == input && baudrate > 0)
+                Console.WriteLine("Avaiable ports:");
+                foreach (var x in SerialPort.GetPortNames()) //pobranie dostępnych portów
                 {
-                    Console.Clear();
-                    Console.WriteLine($"Connected to {input}.");
-                    PortLoop(input, baudrate); //uruchomienie metody z parametrem wybranego portu
+                    Console.WriteLine(x);
                 }
+                Console.Write("Provide port: ");
+                var input = Console.ReadLine();
+                Console.Write("Provide baud rate (Default 9600): ");
+                int baudrate;
+                String tmp = Console.ReadLine();
+                if (!int.TryParse(tmp, out baudrate))
+                    baudrate = 9600;
+
+
+                foreach (var x in SerialPort.GetPortNames()) //przeszukanie czy wprowadzony port znajduje sie wsrod dostepnych portow
+                {
+                    if (x == input && baudrate > 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Connected to {input}.");
+                        PortLoop(input, baudrate); //uruchomienie z wybranym portem
+                    }
+                }
+                Console.WriteLine("Didnt find given port/bad baud rate value\nPress enter to try again or type 'quit' to exit program");
+                if (Console.ReadLine().ToLower() == "quit")
+                    break;
             }
-            Console.WriteLine("Didnt find given port/bad baud rate value");
-            Console.ReadLine();
            
         }
-        /// <summary>
-        /// Metoda odpowiedzialna za zczytywanie wiadomości użytkownika i wysyłanie ich do portu
-        /// </summary>
-        /// <param name="com">Port com.</param>
+
         public static void PortLoop(string com, int baudrate)
         {
-            SerialPort _serialPort = new SerialPort(com, 9600); //utworzenie portu (comport, baudrate)
-            _serialPort.DtrEnable = true; //Data Terminal Ready
-            _serialPort.RtsEnable = true; //Request to send
-            _serialPort.DataBits = 8; //rozmiar elementu
-            _serialPort.Handshake = Handshake.None;
-            _serialPort.Parity = Parity.None;
-            _serialPort.StopBits = StopBits.One; //bit stopu = 1
-            _serialPort.NewLine = System.Environment.NewLine;
+            SerialPort _serialPort = new SerialPort(com, baudrate) //utworzenie portu (comport, baudrate)
+            {
+                DtrEnable = true, //Data Terminal Ready
+                RtsEnable = true, //Request to send
+                DataBits = 8, //rozmiar elementu
+                Handshake = Handshake.None,
+                Parity = Parity.None,
+                StopBits = StopBits.One, //bit stopu = 1
+                NewLine = System.Environment.NewLine
+            }; 
             _serialPort.DataReceived += (s, e) => { //Event odebrania danych
                 SerialPort sp = (SerialPort)s;
                 byte[] packOfBytes = new byte[sp.BytesToRead]; //odczytana wiadomosc do tablicy bajtow
@@ -63,8 +66,8 @@ namespace ConsoleApplication3
                 var msg = Console.ReadLine();
                 if (msg == "quit")
                 {
-                    _serialPort.Close(); //jesli zostanie wpisany quit to zamknij port...
-                    System.Environment.Exit(1); // ...i program
+                    _serialPort.Close(); //zamkniecie portu
+                    System.Environment.Exit(1); // zamkniecie programu
                 }
                 _serialPort.WriteLine(msg); //wyslij wiadomosc do portu
             }
